e712956608240edb9748c6f67d793d97b51c22a5
Jean-Samuel Faubert <jean-samuel.faubert@symbolitech.com>
2026-02-06 06:29:54 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks/{task}/markAsCompleted', [TaskController::class, 'markAsCompleted']);
+    Route::post('/tasks/{task}/markAsNotCompleted', [TaskController::class, 'markAsNotCompleted']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+    Route::put('/tasks/{task}', [TaskController::class, 'update']);
+
+    Route::view('/logout', Logout::class);
+});
+
+Route::middleware('guest')->group(function () {
+    Route::post('/register', Register::class);
+    Route::view('/login', 'auth.login');
+    Route::post('/login', Login::class);
+});
